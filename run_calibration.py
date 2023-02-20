497fd99ac8f71a9916f4e7d164e67b2c03240ee3
Robyn Stuart <robyna.s@gmail.com>
2023-02-20 14:11:17 -0800

    add preloaded example

--- run_calibration.py
+++ run_calibration.py
@@ -10,6 +10,8 @@ import hpvsim as hpv    # For running HPVsim
 import numpy as np      # For numerics
 import pandas as pd     # For data wrangling
 import sciris as sc     # For utilities
+import pylab as pl      # For plotting
+import seaborn as sns   # For plotting
 
 
 #%% Set up a calibration example
@@ -28,20 +30,24 @@ def run_calibration():
     # whereas those in the genotype_pars dictionary do. Both kinds are
     # given in the order [best, lower_bound, upper_bound].
     calib_pars = dict(
-        beta=[0.05, 0.010, 0.20],
+        beta=[0.2, 0.1, 0.3],
         dur_transformed=dict(par1=[5, 3, 10]),
     )
 
-    genotype_pars = dict(
-        hpv16=dict(
-            sev_rate=[0.5, 0.2, 1.0],
-            dur_episomal=dict(par1=[6, 4, 12])
-        ),
-        hpv18=dict(
-            sev_rate=[0.5, 0.2, 1.0],
-            dur_episomal=dict(par1=[6, 4, 12])
-        )
-    )
+    # Set up parameters to vary over genotypes
+    dur_episomal = dict(par1=[4, 3, 10])  # Mean duration of dysplasia (years)
+    sev_rate = [0.3, 0.2, 0.7]  # Logistic growth curve parameter (assumption)
+    sev_infl = [13, 8, 20]
+    transform_prob = [0.0001, 0.0001, 0.001]
+
+    genotypes = ['hpv16', 'hpv18', 'hrhpv']
+    genotype_pars = dict()
+    for gtype in genotypes:
+        genotype_pars[gtype] = dict()
+        genotype_pars[gtype]['sev_rate'] = sev_rate
+        genotype_pars[gtype]['dur_episomal'] = dur_episomal
+        genotype_pars[gtype]['transform_prob'] = transform_prob
+        genotype_pars[gtype]['sev_infl'] = sev_infl
 
     # List the datafiles that contain data that we wish to compare the model to:
     datafiles = ['data/nigeria_cancer_cases.csv',
@@ -66,12 +72,55 @@ def run_calibration():
     return sim, calib
 
 
+def run_premade_calibration():
+    '''
+    Run a premade calibration for Nigeria
+    '''
+
+    # Parameters
+    pars = dict(
+        n_agents=50e3,
+        dt=0.25,
+        start=1950,
+        end=2020,
+        location='nigeria',
+        debut= dict(
+            f=dict(dist='normal', par1=14.8, par2=2.),
+            m=dict(dist='normal', par1=17.0, par2=2.)),
+        genotypes=[16, 18, 'hrhpv'],
+        condoms=dict(m=0.01, c=0.1, o=0.2),
+        eff_condoms=0.5,
+        ms_agent_ratio=100,
+    )
+
+    # Make sim
+    sim = hpv.Sim(pars=pars)
+
+    # Load pre-made calibration parameters
+    file = 'results/nigeria_pars_prerun.obj'
+    calib_pars = sc.loadobj(file)
+
+    # Initialize the sim, then update the parameters
+    sim.initialize()
+    sim.update_pars(calib_pars)
+
+    # Run and plot
+    sim.run()
+    sim.plot()
+
+    return sim
+
+
 #%% Run as a script
 if __name__ == '__main__':
 
     T = sc.tic()
 
-    sim, calib = run_calibration()
+    run_calibration = False
+    load_calibration = True
+
+    if run_calibration: sim, calib = run_calibration()
+    if load_calibration: sim = run_premade_calibration()
 
     sc.toc(T)
     print('Done.')
